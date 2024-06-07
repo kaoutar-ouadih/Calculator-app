c869baa6917aaee7356d93a8015ea9d78cabb1a2
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-06-07 16:46:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'calculator-application';
+  result : any = '0';
+  clickedValue : string = 'noValue';
+
+
+  onBtnClick(value : string){
+    if(value == "="){
+      this.result = eval(this.result);   
+      this.clickedValue = this.result;
+      if(this.isFloat(this.result)){
+        this.result = this.result.toFixed(3);
+      }
+    }
+    else if(value == "RESET"){
+      this.result = 0;   
+      this.clickedValue = 'noValue';
+    }
+    else if(value == "DEL"){
+      this.clickedValue = this.clickedValue.slice(0, -1);
+      console.log(this.clickedValue);
+      this.result = this.clickedValue;
+    }
+    else{
+      this.onNumberClick(value);
+    }
+  }
+
+  onNumberClick(value : string){
+    if(this.clickedValue == 'noValue'){
+      this.clickedValue = value;
+    }else{
+      this.clickedValue += value;
+    }
+    this.result = this.clickedValue;
+  }
+  isFloat(num) {
+    return typeof num === 'number' && num % 1 !== 0;
+}
 }
